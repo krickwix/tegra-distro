40b95be2c7053fda4acbe31627b38df69f34103b
Fabien Andrieux <fandrieu@cisco.com>
2022-09-09 15:02:49 +0000

    usb-mount: retrieve env variables from the config file. write back a bunch of journals and logs

--- layers/meta-greatbear/recipes-connectivity/agent/files/usb-mount.sh
+++ layers/meta-greatbear/recipes-connectivity/agent/files/usb-mount.sh
@@ -15,3 +15,55 @@ case "${ACTION}" in
         if [[ -n ${MOUNT_POINT} ]]; then /bin/umount -l ${DEVICE}; fi
         ;;
 esac
+
+
+FILE=config.gb
+# Search for a GB config file
+if test -f "/media/$FILE"; then
+   while IFS="=" read -r key value; do
+    case "$key" in
+      "http_proxy") echo "$key=$value";;
+      "https_proxy") echo "$key=$value";;
+      "no_proxy") echo "$key=$value";;
+    esac
+  done < /media/$FILE
+fi
+
+. /media/$FILE
+
+MANIFESTS_DIR=/var/lib/rancher/k3s/server/manifests
+EDGE_OPERATOR_SECRET="gbear-edge-operator-env"
+
+sudo mkdir -p $MANIFESTS_DIR
+sudo chmod 700 $MANIFESTS_DIR
+
+if [ -n "$https_proxy" ]; then
+    proxy_settings="  http_proxy: $(echo -n "$http_proxy" | base64 -w0)
+  https_proxy: $(echo -n "$https_proxy" | base64 -w0)
+  no_proxy: $(echo -n "$no_proxy" | base64 -w0)"
+fi
+
+sudo tee $MANIFESTS_DIR/gbear-edge-env.yaml > /dev/null << EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: gbear
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  namespace: gbear
+  name: $EDGE_OPERATOR_SECRET
+data:
+  NODEMANAGER_TOKEN: $(echo -n $API_TOKEN | base64 -w0)
+  NODEMANAGER_URL: $(echo -n $API_URL | base64 -w0)
+  CLUSTER_ID: $(echo -n $CLUSTER_ID | base64 -w0)
+  NODEMANAGER_TENANT_ID: $(echo -n $TENANT_ID | base64 -w0)
+$proxy_settings
+EOF
+# Dump logs to drive
+logdate=$(hostname)-$(date "+%F%H%M%S")
+logfile=journal-$logdate
+journalctl -x --no-pager > /tmp/$logfile && gzip /tmp/$logfile && mv -v /tmp/$logfile.gz /media
+tar zcvf /media/gbear-agent-states-$logdate.tgz /var/lib/gbear
+kubectl get all -A -owide > /tmp/kubectl-$logdate && gzip /tmp/kubectl-$logdate && mv -v /tmp/kubectl-$logdate.gz /media
